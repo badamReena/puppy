92a33c3174c9639409cb0dd716178803bf68dff2
badamReena <reenajayaraman31@gmail.com>
2024-10-16 14:26:43 +0530

    drop by drop,the bucket gets filled

--- puppy.go
+++ puppy.go
@@ -1,9 +1,30 @@
 package puppy
 
+import (
+	"fmt"
+
+	"github.com/badamReena/dog"
+)
+
 func Bark() string {
-	return "Woof"
+	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
+
+}
+func From11() {
+	fmt.Println("I'm from version 1.1.0")
+
+}
+func From12() {
+	fmt.Println("I'm from version 1.2.0")
 }
